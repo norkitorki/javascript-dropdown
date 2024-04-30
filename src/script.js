2772489c1e8c73e64db6315aae6de0ccaad1a3e0
norkitorki <norki@tuta.io>
2024-04-30 11:57:10 +0200

    Expand example menu functionality

--- src/script.js
+++ src/script.js
@@ -4,6 +4,6 @@ const clickableMenu = document.querySelector('.dropdownMenuClick');
 const focusableMenu = document.querySelector('.dropdownMenuFocus');
 const hoverableMenu = document.querySelector('.dropdownMenuHover');
 
-dropdown(clickableMenu, { click: true });
-dropdown(focusableMenu, { focus: true, hover: true });
+dropdown(clickableMenu, { click: true, retractAfterClick: true });
+dropdown(focusableMenu, { focus: true, hover: true, retractAfterClick: true });
 dropdown(hoverableMenu, { click: true, hover: true });
