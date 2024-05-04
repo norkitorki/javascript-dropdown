59f3a5550e848e145a3941a095b7e42f7e30c2a7
norkitorki <norki@tuta.io>
2024-05-04 10:59:58 +0200

    Remove npm dependencies

--- src/script.js
+++
@@ -1,9 +0,0 @@
-import { dropdown } from './dropdown';
-
-const clickableMenu = document.querySelector('.dropdownMenuClick');
-const focusableMenu = document.querySelector('.dropdownMenuFocus');
-const hoverableMenu = document.querySelector('.dropdownMenuHover');
-
-dropdown(clickableMenu, { click: true, retractAfterClick: true });
-dropdown(focusableMenu, { focus: true, hover: true, retractAfterClick: true });
-dropdown(hoverableMenu, { click: true, hover: true });
